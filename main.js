b373786b6e3473400719a1ad237f9a18a2948637
Christina Woodhams <cwoodhams89@gmail.com>
2022-09-23 20:07:54 -0400

    second commit

--- main.js
+++ main.js
@@ -12,7 +12,7 @@ const newAgeInput = document.querySelector('#age')
 const newLikesText = document.querySelector('textarea')
 const charContainer = document.querySelector('section')
 
-// const baseURL = 
+const baseURL = `http://localhost:4000`
 
 function createCharacterCard(char) {
   let charCard = document.createElement('div')
@@ -31,3 +31,91 @@ function createCharacterCard(char) {
 function clearCharacters() {
   charContainer.innerHTML = ``
 }
+
+function getAllChars() {
+  clearCharacters()
+
+  axios.get(`${baseURL}/characters`)
+    .then(function(res) {
+      for (let i = 0; i < res.data.length; i++) {
+        createCharacterCard(res.data[i])
+      }
+    })
+    .catch(err => console.log(err))
+}
+getAllBtn.addEventListener('click', getAllChars)
+
+getAllChars();
+
+function getOneChar(event){
+  // console.log(event.target.id)
+  axios
+    .get(`${baseURL}/character/${event.target.id}`)
+    .then(res => {
+      clearCharacters();
+      createCharacterCard(res.data)
+    })
+    .catch(err => console.log(err));
+}
+
+for(let i = 0; i < charBtns.length; i++){
+ charBtns[i].addEventListener('click', getOneChar)
+}
+
+function getOldChars(event){
+  event.preventDefault();
+  clearCharacters();
+  
+  axios
+    .get(baseURL + '/character?age=' + ageInput.value)
+    .then(res => {
+      for (let i = 0; i < res.data.length; i++){
+        createCharacterCard(res.data[i])
+      }
+    })
+    .catch(err => console.log(err));
+}
+
+ageForm.addEventListener('submit', getOldChars)
+
+function createNewChar(event) {
+  event.preventDefault()
+
+  clearCharacters()
+
+  let newLikes = [...newLikesText.value.split(',')]
+
+  let body = {
+    firstName: newFirstInput.value, 
+    lastName: newLastInput.value, 
+    gender: newGenderDropDown.value, 
+    age: newAgeInput.value, 
+    likes: newLikes
+  }
+
+  axios.post(`${baseURL}/character`, body) 
+    .then(function(res) {
+      for (let i = 0; i < res.data.length; i++) {
+        createCharacterCard(res.data[i])
+      }
+    })
+  
+  newFirstInput.value = ''
+  newLastInput.value = ''
+  newGenderDropDown.value = 'female'
+  newAgeInput.value = ''
+  newLikesText.value = ''
+}
+
+
+getAllBtn.addEventListener('click', getAllChars)
+
+for (let i = 0; i < charBtns.length; i++) {
+  charBtns[i].addEventListener('click', getOneChar)
+}
+
+ageForm.addEventListener('submit', getOldChars)
+
+createForm.addEventListener('submit', createNewChar)
+
+getAllChars()
